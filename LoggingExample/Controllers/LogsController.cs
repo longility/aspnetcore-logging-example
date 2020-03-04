d12c759ccda7d87e2ac9e770379287a49a047d96
Long Mai <longility@gmail.com>
2020-03-04 07:02:52 -0600

    feat: initial

---
+++ LoggingExample/Controllers/LogsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LoggingExample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LogsController : ControllerBase
+    {
+        private readonly ILogger<LogsController> logger;
+
+        public LogsController(ILogger<LogsController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Log()
+        {
+            logger.LogCritical("Hello world");
+            return NoContent();
+        }
+    }
+}
---
+++ LoggingExample/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Formatting.Elasticsearch;
+
+namespace LoggingExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSerilog((ctx, cfg) =>
+                    {
+                        cfg.WriteTo.Console(new ElasticsearchJsonFormatter());
+                    });
+                });
+    }
+}
